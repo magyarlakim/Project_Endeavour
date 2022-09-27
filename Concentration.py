483682fe66c95417634b90ae78cd0a7962fde942
Miklos Magyarlaki <magyarlakimiklos@gmail.com>
2022-09-27 20:51:34 +0200

    Updated

--- Concentration.py
+++ Concentration.py
@@ -1,5 +1,10 @@
 """
 This script is responsible for calculating concentration on a given entropy
 """
-
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
 print("Hello world")
+
