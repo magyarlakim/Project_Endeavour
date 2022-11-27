8e66d883247fc66b83bf9f258ea6056515454e93
Miklos Magyarlaki <magyarlakimiklos@gmail.com>
2022-11-27 21:09:35 +0100

    Updated JS functions+ css

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,7 @@ console.log(fruitProcessor(2, 3));
 console.log(cutFruitPieces(6));
 
 const calcAge = function(birthYear){
-  return 2037 - birthYear;
+  return 2022 - birthYear;
 }
 
 const yearsUntilRetirement = function (birthYear, firstName) {
@@ -24,12 +24,52 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
   if (retirement > 0) {
     console.log(`${firstName} retires in ${retirement} years`);
+    document.getElementById('content').innerHTML = `${firstName} retires in ${retirement} years`
     return retirement;
   } else {
     console.log(`${firstName} has already retired 🎉`);
+    document.getElementById('content').innerHTML = `${firstName} has already retired 🎉`
     return -1;
   }
 }
 
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
+// updated function with ability to change
+
+const yearsUntilRetirement_mod = function (firstName) {
+  const birthYear = document.getElementById('birthYearRange').value;
+  
+  //alert(birthYear);
+  const age = calcAge(birthYear);
+  //alert(age);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    document.getElementById('content').innerHTML = `${firstName} retire in ${retirement} years`
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    document.getElementById('content').innerHTML = `${firstName} have already retired 🎉`
+    return -1;
+  }
+}
+
+const get_portfolio_type = function(){
+  const birthYear = document.getElementById('birthYearRange').value;
+  const Notional= 1000000
+  //calculate Age;
+  const age = calcAge(birthYear);
+  // feed in the preference
+  const preference = document.getElementById('temp').value;
+
+  if ( age>50 && preference<20) {
+    portfolio_proceeds= Notional*1.03
+  } else {
+    portfolio_proceeds= Notional*2
+  }
+  document.getElementById('portfolio_proceeds').innerHTML= `FV is:  ${portfolio_proceeds}.`;
+  return portfolio_proceeds
+}
+//console.log(yearsUntilRetirement(1991, 'Jonas'));
+//console.log(yearsUntilRetirement(1950, 'Mike'));
+//document.getElementById('content').innerHTML = "trial";
